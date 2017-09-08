e570cc6d702d0af34ba8a4c0eb3299ab3920c6f9
Emerson Demetrio <emerson.demetrio@scytech.com.br>
2017-09-08 10:40:00 -0300

    Clear Copy checking

--- run.sh
+++ run.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 # Branch Removing Tool
 
-printf "Branch Removing Tool\n\n"
+printf "\n[ Branch Removing Tool ] \n\n"
+printf "Removing old branchs from your local copy\n\n"
 
 CUR_BRANCH=`git rev-parse --abbrev-ref HEAD`;
 BRANCHS="`git branch | grep -v 'help' | grep -v 'development' | grep -v 'master' | grep -v '$CUR_BRANCH'`"
 
-printf "Notice: $CUR_BRANCH will not be removed. \n\n"
+printf "> Notice: current branch '$CUR_BRANCH' will not be removed. \n\n"
+
+if [ -z "$BRANCHS" ]; then
+	printf "> No other branchs were found. Local copy is clear\n\n";
+	printf "> Exiting.\n\n"
+	exit;
+fi
+
 
 function remove_branchs() {
 	while read -r branch; do
 		git branch -D $branch
 	done <<< "$BRANCHS"
-	printf "All done. Goodbye. \n\n"
+	printf "> All done. Goodbye. \n\n"
 	exit;
 }
 
